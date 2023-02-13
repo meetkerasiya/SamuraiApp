d7c1001a5f62ef89f87de16c48c9841b795d5cca
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-13 14:11:55 +0530

    context file added

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.UI
+{
+	class Program
+	{
+		private static SamuraiContext _context=new SamuraiContext();
+
+		private static void Main(string[] args)
+		{
+			_context.Database.EnsureCreated();
+			GetSamurais("Before Add:");
+			AddSamurai();
+			GetSamurais("After Add:");
+			Console.WriteLine("Press any key...");
+			Console.ReadKey();
+		}
+
+        private static void AddSamurai()
+        {
+			//var samurai = new Samurai { Name = "Kin'emon" };
+			var samurai = new Samurai { Name = "Nekomumashi" };
+			_context.Samurais.Add(samurai);
+			_context.SaveChanges();
+
+
+		}
+
+		private static void GetSamurais(string v)
+        {
+            var samurais=_context.Samurais.ToList();
+			Console.WriteLine($"Samurai cont is {samurais.Count}");
+			foreach( var samurai in samurais )
+			{
+				Console.WriteLine(samurai.Name);
+			}
+        }
+    }
+}
