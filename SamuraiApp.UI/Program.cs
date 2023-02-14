a0c041715905d5a0f4599f6ec79f62379ec3d1d3
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-14 11:12:10 +0530

    add and get functionality modified

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -1,4 +1,5 @@
-﻿using SamuraiApp.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.UI
@@ -9,27 +10,31 @@ namespace SamuraiApp.UI
 
 		private static void Main(string[] args)
 		{
-			_context.Database.EnsureCreated();
-			GetSamurais("Before Add:");
-			AddSamurai();
-			GetSamurais("After Add:");
+			//_context.Database.EnsureCreated();
+
+			AddSamurai("Kin'emon","raizo","kawamatsu");
+			GetSamurais();
 			Console.WriteLine("Press any key...");
 			Console.ReadKey();
 		}
 
-        private static void AddSamurai()
+        private static void AddSamurai(params string[] names)
         {
-			//var samurai = new Samurai { Name = "Kin'emon" };
-			var samurai = new Samurai { Name = "Nekomumashi" };
-			_context.Samurais.Add(samurai);
+			
+			foreach(string name in names)
+			{
+				_context.Samurais.Add(new Samurai { Name= name } );	
+			}
 			_context.SaveChanges();
 
 
 		}
 
-		private static void GetSamurais(string v)
+		private static void GetSamurais()
         {
-            var samurais=_context.Samurais.ToList();
+            var samurais=_context.Samurais
+				.TagWith("ConsoleApp.Program.GetSamurais method")
+				.ToList();
 			Console.WriteLine($"Samurai cont is {samurais.Count}");
 			foreach( var samurai in samurais )
 			{
